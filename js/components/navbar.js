b323762295e5f9645750811f5ea42633dca8f0fc
Tolga Erdonmez <tolgaerdonmez@gmail.com>
2019-12-29 01:58:35 +0300

    dev tolga
    hackathon, hakkimizda, iletisim sayfalarinin temelleri atildi.

--- js/components/navbar.js
+++ js/components/navbar.js
@@ -1,66 +1,70 @@
 class Navbar extends React.Component {
-	constructor(props) {
-		super(props);
-	}
+    constructor(props) {
+        super(props);
+    }
 
-	componentDidMount(){
-		// initilizating sidenav for mobile
-		var elems = document.querySelectorAll('.sidenav');
-		var instances = M.Sidenav.init(elems);
-	}
+    componentDidMount() {
+        // initilizating sidenav for mobile
+        var elems = document.querySelectorAll(".sidenav");
+        var instances = M.Sidenav.init(elems);
+    }
 
-	render() {
-		return (
-			<nav class="grey darken-4 " role="navigation">
-				<div class="nav-wrapper container">
-					<a id="logo-container" href="#" class="brand-logo">
-						cal&lt;<span>hackathon</span>&gt;
-					</a>
-						{/* <img src="logo.png" alt="btlogo" height="100" class="brand-logo" id="logo-container"/> */}
+    render() {
+        return (
+            <nav class="grey darken-4 " role="navigation">
+                <div class="nav-wrapper container">
+                    <a id="logo-container" href="#" class="brand-logo">
+                        cal&lt;<span>hackathon</span>&gt;
+                    </a>
+                    {/* <img src="logo.png" alt="btlogo" height="100" class="brand-logo" id="logo-container"/> */}
 
-					{/* desktop navbar */}
-					<ul class="right hide-on-med-and-down">
-						<li>
-							<a href="index.html">Anasayfa</a>
-						</li>
-						<li>
-							<a href="hackathon.html">Yarışma</a>
-						</li>
-						<li>
-							<a href="about.html">Hakkımızda</a>
-						</li>
-						<li>
-							<a href="contact.html">İletişim</a>
-						</li>
-						<li>
-							<a href="#">Başvur !</a>
-						</li>
-					</ul>
-					{/* mobile navbar */}
-					<ul id="nav-mobile" class="sidenav">
-						<li>
-							<a href="index.html">Anasayfa</a>
-						</li>
-						<li>
-							<a href="hackathon.html">Yarışma</a>
-						</li>
-						<li>
-							<a href="about.html">Hakkımızda</a>
-						</li>
-						<li>
-							<a href="contact.html">İletişim</a>
-						</li>
-						<li>
-							<a href="#">Başvur !</a>
-						</li>
-					</ul>
-					<a href="#" data-target="nav-mobile" class="sidenav-trigger">
-						<i class="material-icons">menu</i>
-					</a>
-				</div>
-			</nav>
-		);
-	}
+                    {/* desktop navbar */}
+                    <ul class="right hide-on-med-and-down">
+                        <li>
+                            <a href="index.html">Anasayfa</a>
+                        </li>
+                        <li>
+                            <a href="hackathon.html">Hackathon</a>
+                        </li>
+                        <li>
+                            <a href="about.html">Hakkımızda</a>
+                        </li>
+                        <li>
+                            <a href="contact.html">İletişim</a>
+                        </li>
+                        <li>
+                            <a href="#">Başvur !</a>
+                        </li>
+                    </ul>
+                    {/* mobile navbar */}
+                    <ul id="nav-mobile" class="sidenav">
+                        <li>
+                            <a href="index.html">Anasayfa</a>
+                        </li>
+                        <li>
+                            <a href="hackathon.html">Hackathon</a>
+                        </li>
+                        <li>
+                            <a href="about.html">Hakkımızda</a>
+                        </li>
+                        <li>
+                            <a href="contact.html">İletişim</a>
+                        </li>
+                        <li>
+                            <a href="#">Başvur !</a>
+                        </li>
+                    </ul>
+                    <a
+                        href="#"
+                        data-target="nav-mobile"
+                        class="sidenav-trigger"
+                    >
+                        <i class="material-icons">menu</i>
+                    </a>
+                </div>
+            </nav>
+        );
+    }
 }
 
 const domContainer = document.querySelector("#navbar_container");
