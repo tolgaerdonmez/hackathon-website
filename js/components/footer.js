206440239d8ccff483dccd077bca422b21b09b03
holy-as-god <hexagonguys@gmail.com>
2020-01-14 17:05:37 +0300

    Okulda yapılanlar.

--- js/components/footer.js
+++ js/components/footer.js
@@ -1,78 +1,23 @@
 class Footer extends React.Component {
-	constructor(props) {
-		super(props);
-	}
+    constructor(props) {
+        super(props);
+    }
 
-	render() {
-		return (
-			<footer class="page-footer grey darken-4">
-				<div class="container">
-					<div class="row">
-						<div class="col l6 s12">
-							<h5 class="white-text">CAL Hackathon</h5>
-							<p class="grey-text text-lighten-4">
-								We are a team of college students working on this project like it's our full time job. Any amount would
-								help support and continue development on this project and is greatly appreciated.
-							</p>
-						</div>
-						{/* <div class="col l3 s12">
-							<h5 class="white-text">Settings</h5>
-							<ul>
-								<li>
-									<a class="white-text" href="#!">
-										Link 1
-									</a>
-								</li>
-								<li>
-									<a class="white-text" href="#!">
-										Link 2
-									</a>
-								</li>
-								<li>
-									<a class="white-text" href="#!">
-										Link 3
-									</a>
-								</li>
-								<li>
-									<a class="white-text" href="#!">
-										Link 4
-									</a>
-								</li>
-							</ul>
-						</div> */}
-						{/* <div class="col l3 s12">
-							<h5 class="white-text">Sosyal Medya</h5>
-							<ul>
-								<li>
-									<a class="white-text" href="#!">
-										Link 1
-									</a>
-								</li>
-								<li>
-									<a class="white-text" href="#!">
-										Link 2
-									</a>
-								</li>
-								<li>
-									<a class="white-text" href="#!">
-										Link 3
-									</a>
-								</li>
-								<li>
-									<a class="white-text" href="#!">
-										Link 4
-									</a>
-								</li>
-							</ul>
-						</div> */}
-					</div>
-				</div>
-				<div class="footer-copyright">
-					<div class="container">CAL Hackathon - CALBT 2020</div>
-				</div>
-			</footer>
-		);
-	}
+    render() {
+        return (
+            <footer class="page-footer grey darken-4">
+                {/* <div class="container">
+                    <div class="row">
+                        <div class="col l6 s12">
+                        </div>
+                    </div>
+                </div> */}
+                <div class="footer-copyright">
+                    <div class="container">CAL Hackathon - CALBT 2020</div>
+                </div>
+            </footer>
+        );
+    }
 }
 
 const domContainer = document.querySelector("#footer_container");
